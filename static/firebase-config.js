d4f533e0b2b1f3e1c1f7f61ff376e45724beffd0
Maximiliano Islas Solis <maximilianoislassolis@gmail.com>
2025-05-17 00:42:36 +0900

    firebase-config.js

--- static/firebase-config.js
+++ static/firebase-config.js
@@ -1,15 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 
-const firebaseConfig = {
-  apiKey: "",
-  authDomain: "aitinerary-2f93e.firebaseapp.com",
-  projectId: "aitinerary-2f93e",
-  storageBucket: "aitinerary-2f93e.firebasestorage.app",
-  messagingSenderId: "735103404793",
-  appId: "1:735103404793:web:46f2decc1bb059239e3028",
-  measurementId: "G-ESKL6GZQHJ"
-};
+// Use the config injected by Flask into the HTML
+const firebaseConfig = window.firebaseConfig;
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -18,3 +11,4 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 export { auth };
+
